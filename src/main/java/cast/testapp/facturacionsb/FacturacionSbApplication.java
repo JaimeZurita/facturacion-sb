43b91164b2cc75edec4d7a13fbdb1dd3ec190c4d
jzurita <jaime.zurita@mzate.com>
2020-06-05 19:01:13 -0400

    Initial commit

---
+++ src/main/java/cast/testapp/facturacionsb/FacturacionSbApplication.java
@@ -0,0 +1,13 @@
+package cast.testapp.facturacionsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cast/testapp/facturacionsb/FacturacionSbApplicationTests.java
@@ -0,0 +1,13 @@
+package cast.testapp.facturacionsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
