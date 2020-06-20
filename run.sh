1ea5e5bfcf6544394082c25e92284a9e6121d5bd
Jaime Zurita <jaime.zurita@mzate.com>
2020-06-20 10:16:27 -0400

    Cambio de puertos y agregado de --rm al run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker container rm facturacionApp
 
-docker run --publish 8081:8081 -d  --name facturacionApp facturacion-sb
+docker run --publish 8180:8080 -d --rm --name facturacionApp facturacion-sb
